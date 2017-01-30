86d5a2a263b94e64191f7731e892f793d6952e12
SBKubric <sbkubric@gmail.com>
2017-01-30 20:48:12 +0300

    Fixed bad URLs. It is working now.

--- generate.py
+++ generate.py
@@ -4,14 +4,15 @@ import os
 import argparse
 import json
 
-HTML_ARTICLE_DIR = './site/articles/'
-INDEX_PAGE_LOC = './site/index.html'
+BASE_DIR = './site/'
+CONFIG_PATH = './config.json'
+HTML_ARTICLE_DIR = './articles/'
+INDEX_PAGE_PATH = './index.html'
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='The script creates a static encyclopedia website from json config '
                                                  'file and a folder ./articles with Jinja2 template engine.')
-    parser.add_argument('config', help='Path to JSON config file.')
     return parser.parse_args()
 
 
@@ -28,7 +29,7 @@ def get_templates():
     return env.get_template('article_template.html'), env.get_template('index_template.html')
 
 
-def make_html_path(source_path):
+def make_html_url(source_path):
     html_path = '{}{}.html'.format(HTML_ARTICLE_DIR, source_path.split('.')[0])
     return html_path
 
@@ -38,23 +39,27 @@ def get_html_from_markdown(path):
         return markdown(md_handler.read(), extensions=['codehilite', 'fenced_code'])
 
 
-def make_dir_path(path):
+def get_dir_path(path):
     dirs = path.split('/')[:-1]
     return '/'.join(dirs)
+
+
+def get_file_path(file_url):
+    return os.path.normpath('{}{}'.format(BASE_DIR, file_url))
 
 
 def generate_html_articles(template, config):
 
     for article in config['articles']:
         article_path = './articles/{}'.format(article['source'])
-        html_path = make_html_path(article['source'])
-        dir_path = make_dir_path(html_path)
+        html_path = get_file_path(make_html_url(article['source']))
+        dir_path = get_dir_path(html_path)
         check_dir(dir_path)
         article_html = get_html_from_markdown(article_path)
         data = {
             'name': article['title'],
             'topic': article['topic'],
-            'index_path': INDEX_PAGE_LOC,
+            'index_path': '../../index.html',
             'html': article_html,
         }
         with open(html_path, mode='w', encoding='utf-8') as html_source:
@@ -62,14 +67,14 @@ def generate_html_articles(template, config):
 
 
 def generate_index_file(template, config):
-    template.globals['make_html_path'] = make_html_path
+    template.globals['make_html_path'] = make_html_url
     data = {
         'name': 'Содержание',
         'topics': config['topics'],
-        'index_path': INDEX_PAGE_LOC,
+        'index_path': INDEX_PAGE_PATH,
         'articles': config['articles'],
     }
-    with open(INDEX_PAGE_LOC, mode='w', encoding='utf-8') as html_source:
+    with open(get_file_path(INDEX_PAGE_PATH), mode='w', encoding='utf-8') as html_source:
         html_source.write(template.render(data))
 
 
@@ -79,10 +84,9 @@ def load_config(config_file_path):
 
 
 if __name__=='__main__':
-
     args = parse_args()
     article_template, index_template = get_templates()
-    config_json = load_config(args.config)
+    config_json = load_config(CONFIG_PATH)
     generate_html_articles(article_template, config_json)
     generate_index_file(index_template, config_json)
 
